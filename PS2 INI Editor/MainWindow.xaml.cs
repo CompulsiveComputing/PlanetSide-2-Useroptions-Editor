6c05496c9dd4249e9f57164d1e61e4d45e966705
kiseido <brettseguin@gmail.com>
2015-01-13 20:24:43 -0800

    Restarted work

--- PS2 INI Editor/MainWindow.xaml.cs
+++ PS2 INI Editor/MainWindow.xaml.cs
@@ -20,6 +20,39 @@ namespace PS2_INI_Editor
     /// </summary>
     public partial class MainWindow : Window
     {
+    	
+    	private string _applicationName = "PlanetSide 2 Useroptions Editor";
+		private string _applicationShortName = "PS2 INI Editer";
+		private string _versionString = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+		
+		//private int _defaultIncrimentalBackupsToKeep = 10;
+		//private string _userOptionsLocation = "";
+		//private string _backupFolderLocation = "";
+		//private string _valueCatalogueLocation = "";
+		//private int _incrimentalBackupsToKeep;
+		
+		//private string _activeFileName = "";
+		
+		private static string _currentWorkDirectory = System.AppDomain.CurrentDomain.BaseDirectory.ToString();
+		private static string _currentRootDirectory = Directory.GetDirectoryRoot(_currentWorkDirectory).ToString();
+		private string _valueCatalogueName = "ps2useroptionscatalogue.xml";
+		
+		private UserOptionsINI _currentUserOptionsINI;
+		
+		private List<UserOptionsINI> _backupINIlist = new List<UserOptionsINI>();
+		
+		private string _activeSection = "",_activeValue = "";
+		int _activeSectionNumber = -1, _activeValueNumber = -1;		
+		
+		private string[] _defaultGamePaths =
+		{
+			"Program Files (x86)\\Steam\\steamapps\\common\\PlanetSide 2\\",
+			"Program Files\\Steam\\steamapps\\common\\PlanetSide 2\\",
+			"Program Files (x86)\\PlanetSide 2\\",
+			"Users\\Public\\Sony Online Entertainment\\Installed Games\\PlanetSide 2\\",
+			"Planetside 2\\"
+		};
+		
         public MainWindow()
         {
             InitializeComponent();
